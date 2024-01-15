6545158a945a7d6585ad4e4dc628dad7998d08a9
AdielTuson <adielt81@gmail.com>
2024-01-15 20:20:43 +0200

    Create project skelaton

---
+++ script.js
@@ -0,0 +1,30 @@
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+    this.info = function (){
+        return (title + ', ' + author + ', ' + pages + ', ' + haveRead)
+    }
+}
+
+//Code for visualizing
+const firstBook = new Book('Lord of the rings', 'J.R.R Martin', 900, 'Have read');
+const secondBook = new Book('Aragorn', 'Some kid', 500, 'Have read');
+
+addBookToLibrary(firstBook);
+addBookToLibrary(secondBook);
+console.log(myLibrary)
+
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayBooks(bookLibrary) {
+    for (const book of bookLibrary) {
+        console.log(book);
+    }
+}
